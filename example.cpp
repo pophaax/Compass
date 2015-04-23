4202501a353c951b25b0227367fe5f589fc8ade5
jsalmen <joakim,salmen@ha.ax>
2015-04-23 12:58:50 +0000

    Got mocking to work in example.

--- example.cpp
+++ example.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
-#include "HMC6343.h"
+#include "Compass.h"
+#include "MockCompass.h"
 
 int main() {
 	std::cout << "Compass example" << std::endl;
 	std::cout << "---------------" << std::endl;
     std::cout << "creating object" << std::endl;
-	HMC6343 compass;
+
+	Compass compass;
+
 	std::cout << "initialization: " << compass.init() << std::endl;
 	std::cout << "read" << std::endl;
 	int i=0;
