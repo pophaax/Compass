f32dd66fa692faf70bbef2b74566d6ae6f8b819b
tsjoblom <120412@ha.ax>
2015-05-21 07:07:24 +0000

    test for compass compensation

--- example.cpp
+++ example.cpp
@@ -3,6 +3,7 @@
 #include "HMC6343.h"
 #include "Compass.h"
 #include "MockCompass.h"
+#include <vectormath_aos.h>
 
 int main() {
 	std::cout << "Compass example" << std::endl;
@@ -14,12 +15,16 @@ int main() {
 	std::cout << "object created" << std::endl;
 
 	std::cout << "initialization: " << compass->init() << std::endl;
-
+	int compHeading = 0;
 	while(true) {
 		compass->readValues();
 		std::cout << "heading : " << compass->getHeading() << "\t"
 				<< "pitch : " << compass->getPitch() << "\t"
 				<< "roll : " << compass->getRoll() << std::endl;
+		compHeading = Vectormath::Aos::Quat::Quat(0,0,1, compass->getRoll()) *
+				Vectormath::Aos::Quat::Quat(1,0,0, compass->getPitch()) *
+				Vectormath::Aos::Quat::Quat(0,1,0, compass->getRoll());
+		std::cout << "compensated heading : "<<compHeading<< std::endl;
 		usleep(2000);
 	}
 }
